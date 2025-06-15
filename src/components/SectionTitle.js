be0d5cc84faa5e28ef0d00e077f17c1ea1ddad8b
Julien Lisita <julien.lisita@gmail.com>
2025-06-15 23:57:42 +0200

    feat: add dynamic color support to SectionTitle

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -1,14 +1,26 @@
 // src/components/common/SectionTitle.jsx
 
-export default function SectionTitle({ className, children, id }) {
+export default function SectionTitle({ className, children, id, color="black" }) {
+    
+      const resolveColor = (color) => {
+      if (
+        color.startsWith("#") ||
+        color.startsWith("rgb") ||
+        color.startsWith("hsl")
+      ) {
+        return color;
+      }
+      return `var(--color-${color})`;
+    };
+
     return (
       <h2 
         id={id}
         className={`${className}
-        mt-8 sm:mt-10 lg:mt-12
         text-xl sm:text-2xl lg:text-3xl 
         font-heading text-center font-semibold
         `}
+        style={{ color: resolveColor(color) }}
       >
         {children}
       </h2>
