ab9d4972daabb3423926d033fc1dd392af84e95a
Julien Lisita <julien.lisita@gmail.com>
2025-06-15 23:56:38 +0200

    feat: implement Section component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,23 @@
-export default function Section() {
+// src/components/SectionTitle.js
+
+const resolveColor = (color) => {
+      if (
+        color.startsWith("#") ||
+        color.startsWith("rgb") ||
+        color.startsWith("hsl")
+      ) {
+        return color;
+      }
+      return `var(--color-${color})`;
+    };
+
+export default function Section({className, bgColor="creme", children}) {
   return (
-    <>
-        Section
-    </>
+    <section 
+      className={`${className} px-4 sm:px-6 lg:px-12 py-8 sm:py-10 lg:py-12`}
+      style={{ backgroundColor: resolveColor(bgColor) }}
+    >
+        {children}
+    </section>
   );
 }
