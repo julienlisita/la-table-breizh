30f8861a5871a849fcebca48e1fe58c3144000da
Julien Lisita <julien.lisita@gmail.com>
2025-06-15 23:54:45 +0200

    style: update mobile menu styles in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,11 +16,6 @@ export default function Header() {
         <img src="/images/logo1.png" alt="Logo du restaurant" className="h-14 sm:h-16 lg:h-20 w-auto" />
       </div>
 
-      {/* Nom (centré en tablette & desktop) */}
-      <div className="hidden sm:block sm:text-lg lg:text-xl font-semibold font-heading text-terre bg-clip-text">
-        Le brunch bordelais
-      </div>
-
       {/* Menu desktop */}
       <nav className="hidden lg:flex space-x-6 text-terre font-bold">
         <Link className="border-b-2 border-transparent hover:border-b-2 hover:border-terre" href="/">Accueil</Link>
@@ -47,14 +42,14 @@ export default function Header() {
 
       {/* Menu mobile/tablette déroulant */}
       <div className={[
-        "absolute top-full left-0 w-full bg-terre text-gray-100 flex flex-col items-center space-y-4 overflow-hidden transition-all duration-300 lg:hidden",
+        "absolute top-full left-0 w-full bg-creme text-terre flex flex-col items-center space-y-4 overflow-hidden transition-all duration-300 lg:hidden",
         isOpen ? "max-h-96 opacity-100 translate-y-0 py-4" : "max-h-0 opacity-0 -translate-y-2 py-0"
       ]
         .filter(Boolean)
         .join(" ")}>
-        <Link href="/" onClick={toggleState} className="hover:bg-creme hover:text-terre w-full px-4 py-2 rounded-md text-center transition-all duration-300">Accueil</Link>
-        <Link href="/menu" onClick={toggleState} className="hover:bg-creme hover:text-terre w-full px-4 py-2 rounded-md text-center transition-all duration-300">Menu</Link>
-        <Link href="/contact" onClick={toggleState} className="hover:bg-creme hover:text-terre w-full px-4 py-2 rounded-md text-center transition-all duration-300">Contact</Link>
+        <Link href="/" onClick={toggleState} className="hover:bg-ocre hover:text-creme w-full px-4 py-2 rounded-md text-center transition-all duration-300">Accueil</Link>
+        <Link href="/menu" onClick={toggleState} className="hover:bg-ocre hover:text-creme w-full px-4 py-2 rounded-md text-center transition-all duration-300">Menu</Link>
+        <Link href="/contact" onClick={toggleState} className="hover:bg-ocre hover:text-creme w-full px-4 py-2 rounded-md text-center transition-all duration-300">Contact</Link>
       </div>
     </header>
   );
