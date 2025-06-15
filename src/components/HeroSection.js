2b10cb4741cbd2dea21bca49c86a11b1e0ad011c
Julien Lisita <julien.lisita@gmail.com>
2025-06-15 23:48:31 +0200

    feat: implement HeroSection component

---
+++ src/components/HeroSection.js
@@ -0,0 +1,22 @@
+// src/components/HeroSection.js
+
+import Button from "./Button";
+import PageTitle from "./PageTitle";
+
+export default function HeroSection()
+{
+    return (
+        <section className="h-screen flex flex-col items-center justify-center text-white text-center px-4 space-y-12">
+          <PageTitle>Le brunch Bordelais</PageTitle>
+          <p className="text-creme text-xl sm:text-2xl lg:text-3xl font-tagline font-semibold leading-snug">
+            Brunch maison<br />
+            Produits locaux<br />
+            Ambiance conviviale
+          </p>
+          <div className="flex flex-wrap justify-center gap-4">
+            <Button href="/menu">Voir le menu</Button>
+            <Button href="/contact">Nous contacter</Button>
+          </div>
+        </section>
+    )
+}
