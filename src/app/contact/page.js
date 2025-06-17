a71418d0eb6dd62163b50861275b6da3cfdbf798
Julien Lisita <julien.lisita@gmail.com>
2025-06-17 17:52:20 +0200

    feat(contact-page): implement contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,7 +1,23 @@
+// src/app/contact/page.js
+
+import ContactForm from "@/components/ContactForm";
+import PageTitle from "@/components/PageTitle";
+import Section from "@/components/Section";
+
 export default function ContactPage() {
   return (
     <>
-      <h1>Contact</h1>
+      <PageTitle>Contact</PageTitle>
+         
+      {/* Formulaire de contact*/}
+      <Section bgColor="white">
+        <p className="text-left max-w-2xl mx-auto ">
+          Une demande spéciale, une question ou une idée ? Écrivez-nous ici, on vous répond rapidement.
+        </p>
+        <div className="w-full flex justify-center mt-3 sm:mt-4 lg:mt-5">
+          <ContactForm/>
+        </div>
+      </Section>  
     </>
   );
 }
